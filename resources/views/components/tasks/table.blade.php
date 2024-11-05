87038a14a71f04d9569b0fb533d846d9cd3f5af2
yar101 <alexander.yakimets@mail.ru>
2024-11-05 16:50:00 +0300

    Отображение замка на заблокированной задаче

--- resources/views/components/tasks/table.blade.php
+++ resources/views/components/tasks/table.blade.php
@@ -40,6 +40,10 @@
 
             <th scope="col" class="px-2 py-2 text-center">
             </th>
+
+            <th scope="col" class="px-2 py-2 text-center">
+
+            </th>
         </tr>
         </thead>
         <tbody>
@@ -154,6 +158,22 @@
                             <path stroke-linecap="round" stroke-linejoin="round" d="M10.5 6.75L15 2.25"/>
                         </svg>
                     </a>
+                </td>
+
+                <td>
+                    @if ($task->locked_by && $task->locked_by != auth()->user()->id)
+                        <a href="{{ route('task.edit', $task->id) }}" class="lock-icon flex justify-center border border-red-500/50 bg-red-500/20 p-1 m-1 rounded-md h-8 w-8"
+                              data-task-id="{{ $task->id }}">
+                            <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g
+                                    id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier"
+                                                                                   stroke-linecap="round"
+                                                                                   stroke-linejoin="round"></g><g
+                                    id="SVGRepo_iconCarrier"> <path
+                                        d="M12 14.5V16.5M7 10.0288C7.47142 10 8.05259 10 8.8 10H15.2C15.9474 10 16.5286 10 17 10.0288M7 10.0288C6.41168 10.0647 5.99429 10.1455 5.63803 10.327C5.07354 10.6146 4.6146 11.0735 4.32698 11.638C4 12.2798 4 13.1198 4 14.8V16.2C4 17.8802 4 18.7202 4.32698 19.362C4.6146 19.9265 5.07354 20.3854 5.63803 20.673C6.27976 21 7.11984 21 8.8 21H15.2C16.8802 21 17.7202 21 18.362 20.673C18.9265 20.3854 19.3854 19.9265 19.673 19.362C20 18.7202 20 17.8802 20 16.2V14.8C20 13.1198 20 12.2798 19.673 11.638C19.3854 11.0735 18.9265 10.6146 18.362 10.327C18.0057 10.1455 17.5883 10.0647 17 10.0288M7 10.0288V8C7 5.23858 9.23858 3 12 3C14.7614 3 17 5.23858 17 8V10.0288"
+                                        stroke="#E6484F" stroke-width="2" stroke-linecap="round"
+                                        stroke-linejoin="round"></path> </g></svg>
+                        </a>
+                    @endif
                 </td>
             </tr>
             <x-comments.create-form :task="$task"/>
